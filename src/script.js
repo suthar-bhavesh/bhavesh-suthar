8b1b2d420265a9b0eb3c0586314ef913196559cb
Bhavesh Suthar <officialbhaveshsuthar>
2026-03-15 23:32:48 +0530

    contact form updated

--- src/script.js
+++ src/script.js
@@ -30,4 +30,52 @@ const observer = new IntersectionObserver((entries) => {
 
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".reveal").forEach(el => observer.observe(el));
+
+
+    const MyForm = document.querySelector('.myForm');
+    const Userinput = document.querySelectorAll('.UserField')
+    const Submitbtn = document.querySelector('.BtnSubmit');
+    const Error = document.querySelectorAll('.error');
+
+
+    MyForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let FormValid = true;
+        let FormData = {};
+
+        Userinput.forEach((Inputs) => {
+
+            const Value = Inputs.value.trim();
+            const FieldName = Inputs.getAttribute('name') || Inputs.placeholder;
+
+            if (Value === "") {
+                FormValid = false;
+                Inputs.style.border = "2px solid red";
+            } else {
+                Inputs.style.border = "2px solid green";
+                FormData[FieldName] = Value;
+            };
+
+        });
+
+        if (FormValid) {
+            Error.forEach((Inputerror) => {
+                Inputerror.textContent = "";
+            });
+
+            alert('Form Submitted Successfully');
+            MyForm.reset();
+
+            Userinput.forEach(inputs => inputs.style.border = "1px solid #ccc");
+
+        } else {
+            Error.forEach((Inputerror) => {
+                Inputerror.textContent = "Please fill required ields";
+                Inputerror.style.color = "red";
+            });
+        }
+
+    });
+
 });
